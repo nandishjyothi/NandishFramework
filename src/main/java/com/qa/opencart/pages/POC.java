95f59cb345051dfc59a52ad7f18616cefbb5eed2
nandishjyothi <34865124+nandishjyothi@users.noreply.github.com>
2022-07-26 19:21:17 +0530

    Create POC.java

---
+++ src/main/java/com/qa/opencart/pages/POC.java
@@ -0,0 +1,19 @@
+package com.qa.opencart.pages;
+
+public class POC {
+
+	public static void main(String[] args) {
+		
+		
+		int i=10;
+		int h=20;
+		int sum = i+h;
+    
+    String name ="POC";
+    String id = "122";
+		
+		
+
+	}
+
+}
